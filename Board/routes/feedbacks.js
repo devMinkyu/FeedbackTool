440393b419361e61ecfdef7cf60db674e06421fe
minkyu <minkyu9403@gmail.com>
2018-03-31 14:50:25 +0900

    Web - 한글깨짐 수정

--- Board/routes/feedbacks.js
+++ Board/routes/feedbacks.js
@@ -2,6 +2,8 @@ var express = require('express');
 Feedback = require('../models/Feedback');
 var multer = require('multer');
 var fs = require('fs');
+var Iconv  = require('iconv').Iconv;
+
 var _storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './tmp');
@@ -133,6 +135,12 @@ router.get('/comment', function(req, res) {
     var id = req.param('id');
     var page_num = req.param('page_num');
     var comment = req.param('feedbackComment'); // 피드백 내용
+    // 한글 깨짐 변경
+    check = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+    var iconv = new Iconv('euc-kr', 'utf-8//translit//ignore');
+    var commentBin = new Buffer(comment, 'binary');
+    var commentUtf8 = iconv.convert(commentBin).toString('utf-8');
+    
     var count = 0;
     Feedback.findOne({_id: id}, function(err, feedback){
         if(err) throw err;
@@ -142,7 +150,11 @@ router.get('/comment', function(req, res) {
                 break;
             }
         }
-        feedback.comments.unshift({name:req.user.name, userId:req.user._id, memo: comment, page:page_num, userTeam:feedback.user_Team});
+        if(check.test(comment)){
+            feedback.comments.unshift({name:req.user.name, userId:req.user._id, memo: comment, page:page_num, userTeam:feedback.user_Team});
+        }else{
+            feedback.comments.unshift({name:req.user.name, userId:req.user._id, memo: commentUtf8, page:page_num, userTeam:feedback.user_Team});
+        }
         feedback.save(function(err){
             if(err) throw err;
         });
